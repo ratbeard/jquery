904ef97ed1175b6f899efe9240381c8b0638667d
Scott Hughes <hi@scott-hughes.me>
2010-08-04 14:22:46 +1000

    Fixed #6867: added element check to jQuery.makeArray to avoid treating elements with length property as arrays

--- src/core.js
+++ src/core.js
@@ -591,10 +591,11 @@ jQuery.extend({
 		var ret = results || [];
 
 		if ( array != null ) {
-			// The window, strings (and functions) also have 'length'
+			// The window, strings, functions and some DOM elements
+			// (e.g., <form> and <select> elements) also have 'length'
 			// The extra typeof function check is to prevent crashes
 			// in Safari 2 (See: #3039)
-			if ( array.length == null || typeof array === "string" || jQuery.isFunction(array) || (typeof array !== "function" && array.setInterval) ) {
+			if ( array.length == null || typeof array === "string" || array.nodeName || jQuery.isFunction(array) || (typeof array !== "function" && array.setInterval) ) {
 				push.call( ret, array );
 			} else {
 				jQuery.merge( ret, array );
